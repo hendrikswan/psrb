b22a0a3458f4403900417e9a9cd6402edde1c7d3
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-08-09 14:53:20 +0200

    Validate email address

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -8,6 +8,7 @@ import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
+import validator from 'email-validator';
 
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
@@ -23,7 +24,20 @@ import TextField from 'material-ui/TextField';
 // }
 
 class Login extends React.Component {
+  state = {
+    errorText: null,
+  }
+
+
   login = () => {
+    const email = this.emailField.input.value;
+    if (!validator.validate(email)) {
+      this.setState({
+        errorText: 'Please provide a valid email',
+      });
+      return;
+    }
+
     this.props.login(this.emailField.input.value);
   }
 
@@ -42,14 +56,16 @@ class Login extends React.Component {
     ];
     return (
       <Dialog
-        title="Please provider your email to login"
+        title="Log in with your email"
         actions={actions}
         modal
         open
         contentStyle={{ width: 320 }}
       >
         <TextField
-          hintText="Specify your email"
+          hintText="Your email"
+          errorText={this.state.errorText}
+          floatingLabelText="Your email"
           ref={(f) => { this.emailField = f; }}
         />
       </Dialog>
