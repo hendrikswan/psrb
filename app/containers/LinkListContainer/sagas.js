6f72253aea754c9d0811d913727a1488fa0a8db0
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-08-09 19:48:38 +0200

    Using saga side-effects (call) to declaratively state what effects should happen This closes #12

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -1,20 +1,17 @@
-import { put, select } from 'redux-saga/effects';
+import { put, select, call } from 'redux-saga/effects';
 import { takeLatest } from 'redux-saga';
 import { REQUEST_LINKS, VOTE_LINK } from './constants';
 import { requestLinksSucceeded, requestLinksFailed, requestVoteLinkSucceeded, requestVoteLinkFailed } from './actions';
 import { selectTopic } from '../NavigationContainer/actions';
 import selectLinkListContainer from './selectors';
 import { push } from 'react-router-redux';
+import { fetchLinksFromServer, sendVoteLinkToServer } from '../api';
 
-export function fetchLinksFromServer(topicName) {
-  return fetch(`http://localhost:3000/api/topics/${topicName}/links`)
-    .then(response => response.json());
-}
 
 function* fetchLinks(action) {
   try {
     yield put(selectTopic(action.topicName));
-    const links = yield fetchLinksFromServer(action.topicName);
+    const links = yield call(fetchLinksFromServer, action.topicName);
     yield put(requestLinksSucceeded({
       links,
       topicName: action.topicName,
@@ -25,24 +22,6 @@ function* fetchLinks(action) {
 }
 
 
-export function sendVoteLinkToServer({
-  link,
-  increment,
-  email,
-}) {
-  return fetch(`http://localhost:3000/api/links/${link.id}/vote`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      increment,
-      email,
-    }),
-  }).then(response => response.json());
-}
-
 function* voteLink(action) {
   const state = yield select(selectLinkListContainer());
   if (!state.email) {
@@ -51,7 +30,7 @@ function* voteLink(action) {
   }
 
   try {
-    const updatedLink = yield sendVoteLinkToServer({
+    const updatedLink = yield call(sendVoteLinkToServer, {
       link: action.link,
       increment: action.increment,
       email: state.email,
